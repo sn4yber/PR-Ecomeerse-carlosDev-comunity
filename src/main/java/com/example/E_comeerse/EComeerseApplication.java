1f81c7d4337d0651d6301dfb5c9b09e504b5c1a1
sn4yber <snayber333@gmail.com>
2025-09-16 13:43:02 +0000

    primera subida del proyecto

---
+++ src/main/java/com/example/E_comeerse/EComeerseApplication.java
@@ -0,0 +1,13 @@
+package com.example.E_comeerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComeerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComeerseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/E_comeerse/EComeerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.E_comeerse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComeerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
